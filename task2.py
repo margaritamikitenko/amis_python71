a9fbae9fc3f230b3536db02c582179aff737ebeb
margaritamikitenko <31902599+margaritamikitenko@users.noreply.github.com>
2017-12-22 01:27:37 +0200

    homework_12
    Тема дз: "Рекурсія з параметром"

--- task2.py
+++ task2.py
@@ -1,30 +1,33 @@
-elements = [float(i) for i in input().split]
+elements = [float(i) for i in input().split()]
+el1 = elements
 
-def min_number(elements):
+
+def max_el(elements):
     """
-    Ця функція повертає найменший елемент зі списку elemtnts
+    Ця функція повертає кількість елементів,
+    що дорінюють максимальному елементу
     Args:
-        elemtns: список
-    Returns:
-        elements: дійсне число
-    Examples:
-        >>>print(sum_sequence([4, 6, 7, 3, 9, 9, 0, 0))
-        0.0
-        >>>print(sum_sequence([6, 0, 0, f] ))
-        Traceback (most recent call last):
-        File "C:/Users/User/Desktop/Python/11/task2(11).py",
+        numbers: список
+    Returns: дійсне числa
+    Exampples:
+        >>> print(sum_sequence(2 3 4 7 4 7 7 4))
+        3
+        >>> print(sum_sequence(1 3 4 7 а 7 7 4))
+        File "C:/Users/User/Desktop/Python/12/task2.py",
         line 1, in <listcomp>
-        elemеnts = [float(i) for i in input().split()]
-        ValueError: could not convert string to float: 'f'
+        elements = [float(i) for i in input().split()]
+        ValueError: could not convert string to float: 'а'
     """
-    if len(elements) != 1: 
-        if elements[0] >= elements[1]:
-            return min_number(elements[1:])
-        else:
-            del elements[1]
-            return min_number(elements)
+    if len(elements) == 0:
+        return '0'
     else:
-        return elements[0]
+        if len(elements) > 1:
+            if elements[0] > elements[1]:
+                del elements[1]
+                return max_el(elements)
+            else:
+                return max_el(elements[1:])
+        elif len(elements) == 1:
+            return el1.count(elements[0])
 
-print(min_number(elements))
-
+print(max_el(elements))
